83af831e19b99ac84bcb63dd3397fca9d8dafa40
reddy <reddyaravind05817@gmail.com>
2024-06-06 21:24:35 +0530

    first commit

---
+++ SF04java/src/edu/svit/sf04java/oops/StudentMain.java
@@ -0,0 +1,16 @@
+package edu.svit.sf04java.oops;
+
+public class StudentMain {
+	
+		public static void main(String[] args) {
+			//className objName = new className();
+			Student studentOne = new Student();
+			System.out.println(studentOne);
+			
+			Student studentTwo = new Student(101,"Rahul","CSE","Bandalore");
+			System.out.println(studentTwo);
+		}
+
+	}
+
+
